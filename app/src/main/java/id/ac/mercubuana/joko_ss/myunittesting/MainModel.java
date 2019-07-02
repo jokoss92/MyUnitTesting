467f8010f73386daeccdd9de878a5d00aba1ae10
Joko Sarjono Slameto <johno.smakaduta@gmail.com>
2019-07-02 23:43:01 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/mercubuana/joko_ss/myunittesting/MainModel.java
@@ -0,0 +1,13 @@
+package id.ac.mercubuana.joko_ss.myunittesting;
+
+public class MainModel {
+    private double volume;
+
+    public MainModel(double volume){
+        this.volume = volume;
+    }
+
+    public double getVolume(){
+        return volume;
+    }
+}
---
+++ app/src/main/java/id/ac/mercubuana/joko_ss/myunittesting/MainView.java
@@ -0,0 +1,5 @@
+package id.ac.mercubuana.joko_ss.myunittesting;
+
+public interface MainView {
+    void showVolume(MainModel model);
+}
